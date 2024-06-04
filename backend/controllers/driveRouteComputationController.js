ef0ce0f537d1a61c99dd2774ddddf30ddb9f029c
Miki <vallejomiki30@gmail.com>
2024-06-05 00:21:13 +0800

    added update on longlat

--- backend/controllers/driveRouteComputationController.js
+++ backend/controllers/driveRouteComputationController.js
@@ -152,8 +152,12 @@ const driveCompute = asyncHandler(async (req, res) => {
                 totalTime = totalTime + duration
                 ETA.push({"station_id": station_details[0].id, "station_name": station_details[0].name, "duration": totalTime,"ETA_minutes": minutes, "ETA_seconds": Number(seconds), "distance": distance})
           }
-        if (ETA) {
+        if (ETA && duration <=  120) {
             const update = await Users.findOneAndUpdate({email: email},{"bus_current_station": newArray[0], bus_latitude, bus_longitude},{new:true})
+            res.status(200).json({"data": ETA})
+        }
+        else if (ETA && duration > 120) {
+            const update = await Users.findOneAndUpdate({email: email},{bus_latitude, bus_longitude},{new:true})
             res.status(200).json({"data": ETA})
         }
         else{
@@ -169,6 +173,7 @@ const driveCompute = asyncHandler(async (req, res) => {
 
 export {
     getETA,
+    nextStation,
     driveCompute,
 }
 
