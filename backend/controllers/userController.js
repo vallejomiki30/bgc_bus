b1fed922529fd95bc6218814d94119e3aa895045
Miki <vallejomiki30@gmail.com>
2024-06-05 23:38:18 +0800

    add getuser, alluser, edituser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,57 @@ import Sequence from '../models/sequence.js'
 import Users from '../models/user.js'
 import generateToken from '../util/generateToken.js'
 
+const getUser = asyncHandler(async (req, res) => {
+    const { id } = req.body
+
+    const user = await Users.findOne({id})
+
+    if (user) {
+        res.status(200).json({"data": user})
+    } 
+    else{
+        res.status(401)
+        throw new Error('Invalid User ID')
+    }
+})
+
+const allUser = asyncHandler(async (req, res) => {
+
+    const users = await Users.find({})
+
+    if (users) {
+        res.status(200).json({"data": users})
+    } 
+    else{
+        res.status(401)
+        throw new Error('Invalid User ID')
+    }
+})
+
+const editUser = asyncHandler(async (req, res) => {
+    const { id, firstname, lastname, email, role, route_id, bus_current_station, bus_latitude, bus_longitude } = req.body
+
+    const user = await Users.findOneAndUpdate({id}, {
+        firstname,
+        lastname,
+        email,
+        invisible,
+        role,
+        route_id,
+        bus_current_station,
+        bus_latitude,
+        bus_longitude
+    },{new:true})
+    
+    if(user){
+        res.status(200).json({"data": user})
+    }
+    else{
+        res.status(401)
+        throw new Error('Invalid Credentials')
+    }
+})
+
 const authUser = asyncHandler(async (req, res) => {
     const { email, invisible } = req.body
 
@@ -60,6 +111,9 @@ const addUser = asyncHandler(async (req, res) => {
 })
 
 export {
+    getUser,
+    allUser,
+    editUser,
     authUser,
     addUser,
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import { 
+    getUser,
+    allUser,
     addUser,
     authUser,
+    editUser,
 } from '../controllers/userController.js'
 const router = express.Router()
 
+router.get('/', getUser)
+router.get('/all',allUser)
 router.post('/auth',authUser)
 router.post('/add', addUser)
+router.put('/edit', editUser)
 
 export default router
